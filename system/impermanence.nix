5f7a5f51c4a4ac5d13818b0c6a82e9ead81bedfe
Brad Hesson <brad.hesson@outlook.com>
2024-01-24 01:48:51 -0700

    updated hardware conf

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -2,6 +2,6 @@
   imports = [ flakes.impermanence.nixosModules.impermanence ];
 
   fileSystems."/persist".neededForBoot = true;
-  users.users.bhesson.passwordFile = "/persist/etc/users/bhesson";
-  users.users.root.passwordFile = "/persist/etc/users/root";
+  users.users.bhesson.hashedPasswordFile = "/persist/etc/users/bhesson";
+  users.users.root.hashedPasswordFile = "/persist/etc/users/root";
 }
