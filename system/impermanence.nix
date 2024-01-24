03b8a0a595ea73f7d34ff8f1345c084940a7b97d
Brad Hesson <brad.hesson@outlook.com>
2024-01-24 01:50:44 -0700

    added rollback command

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -1,5 +1,9 @@
-{ flakes, sysargs, ... }: {
+{ flakes, lib, ... }: {
   imports = [ flakes.impermanence.nixosModules.impermanence ];
+
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    zfs rollback -r tank/local/root@blank
+  '';
 
   fileSystems."/persist".neededForBoot = true;
   users.users.bhesson.hashedPasswordFile = "/persist/etc/users/bhesson";
