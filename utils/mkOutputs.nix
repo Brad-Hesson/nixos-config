0cfc4063692ebdbfe565224b00a745bc27b42370
Brad Hesson <brad.hesson@outlook.com>
2024-01-24 04:20:51 -0700

    fixed commands with hm as nixos mod

--- utils/mkOutputs.nix
+++ utils/mkOutputs.nix
@@ -28,96 +28,27 @@ in
       }
     ];
   };
-  homeConfigurations.${username} = flakes.home-manager.lib.homeManagerConfiguration {
-    extraSpecialArgs = { inherit flakes sysargs; };
-    inherit pkgs;
-    modules = userModules ++ [{
-      home.username = username;
-      home.homeDirectory = "/home/${username}";
-      home.stateVersion = "23.05";
-    }];
-  };
-
   apps.${system} =
     let mkScriptApp = import ./mkScriptApp.nix pkgs; in
     {
       default = mkScriptApp "rebuild" ''
         sudo nixos-rebuild switch --flake . && \
-        home-manager switch --flake . && \
-        echo "Rebuild Successful"
-      '';
-      hm = mkScriptApp "hm" ''
-        echo $@
-        home-manager switch --flake . && \
         echo "Rebuild Successful"
       '';
       test = mkScriptApp "test" ''
         sudo nixos-rebuild dry-activate --flake .
-        echo "building the home configuration..."
-        home-manager build --no-out-link --flake . && \
-        echo "would activate the configuration..."
       '';
       gc = mkScriptApp "gc" ''
         sudo nix-collect-garbage
       '';
       history = mkScriptApp "history" ''
-        case $1 in
-          hm | home-manager | h | home)
-            nix profile history --profile ~/.local/state/nix/profiles/home-manager
-          ;;
-          sys | system | s)
-            nix profile history --profile /nix/var/nix/profiles/system
-          ;;
-          *)
-            echo "----------[ System ]----------"
-            nix profile history --profile /nix/var/nix/profiles/system
-            echo ""
-            echo "-------[ Home Manager ]-------"
-            nix profile history --profile ~/.local/state/nix/profiles/home-manager
-          ;;
-        esac
+        nix profile history --profile /nix/var/nix/profiles/system
       '';
       diff = mkScriptApp "diff" ''
-        case $1 in
-          hm | home-manager | h | home)
-            nix profile diff-closures --profile ~/.local/state/nix/profiles/home-manager
-          ;;
-          sys | system | s)
-            nix profile diff-closures --profile /nix/var/nix/profiles/system
-          ;;
-          *)
-            echo "----------[ System ]----------"
-            nix profile diff-closures --profile /nix/var/nix/profiles/system
-            echo ""
-            echo "-------[ Home Manager ]-------"
-            nix profile diff-closures --profile ~/.local/state/nix/profiles/home-manager
-          ;;
-        esac
+        nix profile diff-closures --profile /nix/var/nix/profiles/system
       '';
       wipe = mkScriptApp "wipe" ''
-        case $1 in
-          hm | home-manager | h | home)
-            echo "Home Manager:"
-            nix-env --delete-generations --profile ~/.local/state/nix/profiles/home-manager $2
-            echo ""
-            echo "Profile:"
-            nix-env --delete-generations --profile ~/.local/state/nix/profiles/profile $2
-          ;;
-          sys | system | s)
-            echo "System:"
-            sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system $2
-          ;;
-          *)
-            echo "System:"
-            sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system $1
-            echo ""
-            echo "Home Manager:"
-            nix-env --delete-generations --profile ~/.local/state/nix/profiles/home-manager $1
-            echo ""
-            echo "Profile:"
-            nix-env --delete-generations --profile ~/.local/state/nix/profiles/profile $1
-          ;;
-        esac
+        sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system $1
       '';
 
     };
