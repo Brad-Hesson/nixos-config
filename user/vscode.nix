295c289fb53a37e75cffb3d363cff6da47dc83a4
Brad Hesson <brad.hesson@outlook.com>
2024-01-24 19:50:31 -0700

    dont configure vscode in nix

--- user/vscode.nix
+++ user/vscode.nix
@@ -1,42 +1,4 @@
 { pkgs, ... }: {
-  programs.vscode = {
-    enable = true;
-    userSettings = {
-      extensions.autoUpdate = false;
-      extensions.autoCheckUpdates = false;
-      update.mode = "none";
-      window.zoomLevel = 1;
-
-      nix.enableLanguageServer = true;
-      # ---------- [ nil ] ----------
-      nix.serverPath = "nil";
-      nix.serverSettings = {
-        nil = {
-          formatting = { command = [ "nixpkgs-fmt" ]; };
-        };
-      };
-      # ---------- [ nixd ] ----------
-      # nix.serverPath = "nixd";
-      # nix.serverSettings.nixd = {
-      #   formatting.command = "nixpkgs-fmt";
-      #   eval = {
-      #     target = {
-      #       args = [ "-f" "default.nix" ];
-      #       installable = "homeConfigurations.bhesson";
-      #     };
-      #     depth = 10;
-      #   };
-      #   options = {
-      #     enable = false;
-      #     target.installable = ".#nixosConfigurations.bhesson.options";
-      #   };
-      # };
-    };
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      rust-lang.rust-analyzer
-      usernamehw.errorlens
-    ];
-  };
+  programs.vscode.enable = true;
   home.packages = with pkgs; [ nil nixpkgs-fmt ];
 }
