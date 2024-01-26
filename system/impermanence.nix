9464178ed49730f53ebe7c183bf5af240487881d
Brad Hesson <brad.hesson@outlook.com>
2024-01-25 21:49:12 -0700

    added preemt kernal patch

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -5,6 +5,17 @@
     zfs rollback -r tank/local/root@blank
   '';
   boot.kernelParams = [ "elevator=none" ];
+  boot.kernelPatches = [{
+    name = "enable RT_FULL";
+    patch = null;
+    extraConfig = ''
+      PREEMPT y
+      PREEMPT_BUILD y
+      PREEMPT_VOLUNTARY n
+      PREEMPT_COUNT y
+      PREEMPTION y
+    '';
+  }];
 
   fileSystems."/persist".neededForBoot = true;
   users.users.bhesson.hashedPasswordFile = "/persist/etc/users/bhesson";
