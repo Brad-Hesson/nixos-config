c7765d845c877a7d0ce7c68837e07b3da1b11091
Brad Hesson <brad.hesson@outlook.com>
2024-01-31 18:32:57 -0700

    persisted plasma panels

--- user/plasma5.nix
+++ user/plasma5.nix
@@ -1,3 +1,7 @@
-{flakes, ...}: {
-  imports = [flakes.plasma-manager.homeManagerModules.plasma-manager];
+{ flakes, ... }: {
+  imports = [ flakes.plasma-manager.homeManagerModules.plasma-manager ];
+  home.persistence."/persist/home/bhesson".files = [
+    ".config/plasma-org.kde.plasma.desktop-appletsrc"
+    ".config/plasmashellrc"
+  ];
 }
