e71890f14efa3a57db0636cc4c2c42a47dfcba6c
Brad Hesson <brad.hesson@outlook.com>
2024-02-01 16:00:22 -0700

    added surface kernal modules and thermal mgmt

--- system/ms-surface.nix
+++ system/ms-surface.nix
@@ -1,4 +1,23 @@
-{ flakes, ... }: {
+{ flakes, pkgs, ... }: {
   imports = [ flakes.nixos-hardware.nixosModules.microsoft-surface-common ];
   microsoft-surface.surface-control.enable = true;
+  microsoft-surface.ipts.enable = true;
+
+  services.thermald.enable = true;
+  environment.systemPackages = [ pkgs.auto-cpufreq ];
+  services.auto-cpufreq.enable = true;
+
+  boot = {
+    kernelParams = [ "reboot=pci" ];
+    initrd.kernelModules = [
+      "surface_aggregator"
+      "surface_aggregator_registry"
+      "surface_aggregator_hub"
+      "surface_hid_core"
+      "surface_hid"
+      "intel_lpss"
+      "intel_lpss_pci"
+      "8250_dw"
+    ];
+  };
 }
