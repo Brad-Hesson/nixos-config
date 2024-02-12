0ad6c7700c69ad792dc3242b26b1c98125af78c4
Brad Hesson <brad.hesson@outlook.com>
2024-02-12 02:40:22 -0700

    persisted cargo and rustup cache

--- user/rust.nix
+++ user/rust.nix
@@ -1,11 +1,6 @@
-{ sysargs, flakes, pkgs, ... }:
-let
-  fenix = flakes.fenix.packages.${sysargs.system};
-in
 {
-  home.packages = [
-    fenix.stable.defaultToolchain
-    fenix.stable.rust-analyzer
-    pkgs.gcc
+  home.persistence."/persist/home/bhesson".directories = [
+    ".cargo"
+    ".rustup"
   ];
 }
