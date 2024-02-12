9df2059f6ab6ea5e1bda4cca01bfff845c6c5568
Brad Hesson <brad.hesson@outlook.com>
2024-02-12 02:40:02 -0700

    added direnv

--- user/vscode.nix
+++ user/vscode.nix
@@ -1,10 +1,16 @@
 { pkgs, flakes, ... }: {
   imports = [ flakes.impermanence.nixosModules.home-manager.impermanence ];
   programs.vscode.enable = true;
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
   home.packages = with pkgs; [ nil nixpkgs-fmt ];
 
   home.persistence."/persist/home/bhesson".directories = [
     ".vscode"
     ".config/Code/User"
+    ".local/share/direnv/allow"
   ];
 }
