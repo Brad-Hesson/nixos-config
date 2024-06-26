7bd14acd74ef228636f799ee5b507f922691391d
Brad Hesson <brad.hesson@outlook.com>
2024-06-26 15:58:52 -0600

    error if home-manager not managing bash in firefox app

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -1,4 +1,4 @@
-{
+{ lib, ... }: {
   home-manager.users.bhesson = { config, flakes, ... }: {
     imports = [ flakes.nur.hmModules.nur ];
     programs.firefox = {
@@ -14,9 +14,15 @@
     # make touchscreen scrolling work
     # NOTE: only works if programs.bash.enable = true, meaning
     #       that home-manager is managing the shell
-    home.sessionVariables = {
-      MOZ_USE_XINPUT2 = "1";
-    };
+    # home.sessionVariables = lib.warnIfNot (config.programs.bash.enable == true) "foo is not bar, silly" {
+    home.sessionVariables =
+      lib.trivial.throwIfNot
+        (config.programs.bash.enable == true)
+        "firefox.nix: home-manager setting `programs.bash.enable == true` must be
+        set in order to configure firefox for touchscreens"
+        {
+          MOZ_USE_XINPUT2 = "1";
+        };
     home.persistence."/persist/home/bhesson".directories = [
       ".mozilla"
     ];
