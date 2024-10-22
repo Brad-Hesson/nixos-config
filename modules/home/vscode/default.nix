84eda3b354c21b479c55c8e770340d04b9b603bb
Brad Hesson <brad.hesson@outlook.com>
2024-10-22 01:10:51 -0600

    persist all of vscode

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }: {
+{
   programs.vscode.enable = true;
-  home.packages = with pkgs; [ nil nixpkgs-fmt ];
-
   persistif.directories = [
     ".vscode"
-    ".config/Code/User"
+    ".config/Code"
   ];
 }
