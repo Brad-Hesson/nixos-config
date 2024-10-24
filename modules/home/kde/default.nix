c07d3ffc59dc16ac6fd36c514f742ce39ed5febf
Brad Hesson <brad.hesson@outlook.com>
2024-10-24 14:55:32 -0600

    only configure kde at home level if plasma is enabled at system level

--- modules/home/kde/default.nix
+++ modules/home/kde/default.nix
@@ -1,33 +1,34 @@
-{ inputs, ... }: {
+{ inputs, lib, osConfig, ... }:
+let osCfg = osConfig.mods.display.plasma; in {
   imports = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
-
-  programs.plasma = {
-    enable = true;
-    overrideConfig = true;
-    workspace = {
-      theme = "breeze-dark";
-      colorScheme = "BreezeDark";
-      clickItemTo = "select";
+  config = lib.mkIf (osCfg.enable) {
+    programs.plasma = {
+      enable = true;
+      overrideConfig = true;
+      workspace = {
+        theme = "breeze-dark";
+        colorScheme = "BreezeDark";
+        clickItemTo = "select";
+      };
     };
-  };
 
-  services.kdeconnect = {
-    enable = true;
-    indicator = true;
-  };
-  
+    services.kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
 
-  persistif = {
-    directories = [
-      ".local/share/kwalletd"
-      ".local/share/kscreen"
-      ".local/share/baloo"
-      ".local/share/dolphin"
-    ];
-    files = [
-      ".config/plasma-org.kde.plasma.desktop-appletsrc"
-      ".config/plasmashellrc"
-      ".local/share/user-places.xbel"
-    ];
+    persistif = {
+      directories = [
+        ".local/share/kwalletd"
+        ".local/share/kscreen"
+        ".local/share/baloo"
+        ".local/share/dolphin"
+      ];
+      files = [
+        ".config/plasma-org.kde.plasma.desktop-appletsrc"
+        ".config/plasmashellrc"
+        ".local/share/user-places.xbel"
+      ];
+    };
   };
 }
