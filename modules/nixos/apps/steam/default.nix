370ce27daa9be2792d26d9bb63a4d931fdfe4e28
Brad Hesson <brad.hesson@outlook.com>
2025-04-04 04:52:03 -0600

    added protonGE to steam module

--- modules/nixos/apps/steam/default.nix
+++ modules/nixos/apps/steam/default.nix
@@ -1,9 +1,21 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let cfg = config.mods.apps.steam; in {
   options = {
     mods.apps.steam.enable = lib.mkEnableOption "Steam";
   };
   config = lib.mkIf (cfg.enable) {
     programs.steam.enable = true;
+
+    # Tell steam where to find ProtonGE
+    environment.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+        "\${HOME}/.steam/root/compatibilitytools.d";
+    };
+    environment.systemPackages = with pkgs; [
+      # wine is sometimes needed to install Trackmania
+      wineWowPackages.stable
+      # Command to install latest ProtonGE
+      protonup
+    ];
   };
 }
