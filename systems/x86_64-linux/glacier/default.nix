680be04ed3c103d56ce4f98b0e89385cf69c3560
Brad Hesson <brad.hesson@outlook.com>
2025-04-15 13:14:36 -0600

    default to wayland

--- systems/x86_64-linux/glacier/default.nix
+++ systems/x86_64-linux/glacier/default.nix
@@ -5,7 +5,7 @@
     ./nvidia.nix
   ];
 
-  mods.display.plasma = { enable = true; defaultx11 = true; };
+  mods.display.plasma = { enable = true; defaultx11 = false; };
   mods.apps.steam.enable = true;
 
   impermanence = {
