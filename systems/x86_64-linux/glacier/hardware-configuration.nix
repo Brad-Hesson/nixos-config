910b7c6f28b3421acab1d78fb30aeb229cdc71fa
Brad Hesson <brad.hesson@outlook.com>
2025-07-08 02:57:11 -0600

    update, change hardware-configuration for new hardware

--- systems/x86_64-linux/glacier/hardware-configuration.nix
+++ systems/x86_64-linux/glacier/hardware-configuration.nix
@@ -9,9 +9,9 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
