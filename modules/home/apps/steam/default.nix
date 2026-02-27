f3220753b4d1817e020a71d738869bae8e81398d
Brad Hesson <brad.hesson@outlook.com>
2026-02-26 17:01:51 -0700

    updated and fixed persistif, probably

--- modules/home/apps/steam/default.nix
+++ modules/home/apps/steam/default.nix
@@ -2,11 +2,6 @@
   # Cannot be enabled directly
   # If enabled at the system level, then instructs the persistance of the user steam folder
   config = lib.mkIf (osConfig.mods.apps.steam.enable) {
-    persistif.directories = [
-      {
-        directory = ".local/share/Steam";
-        method = "symlink";
-      }
-    ];
+    persistif.directories = [ ".local/share/Steam" ];
   };
 }
--- modules/nixos/persistif/default.nix
+++ modules/nixos/persistif/default.nix
@@ -1,44 +1,37 @@
-{ lib, inputs, config, pkgs, ... }:
+{ lib, inputs, config, ... }:
 let
   cfg = config.impermanence;
-  # the type of the forwarded impermanence option, extracted from the impermanence module
-  persistOptionType = (pkgs.callPackage inputs.impermanence.nixosModules.impermanence {
-    utils = { };
-    # The extracted option type has deeply nested options that copy their defaults from options in the top
-    # level of the def (eg. enableDebugging, hideMounts). Thus, we need to set the internal top level def
-    # to *our* top level def, so that the option defaults are correct.
-    # See https://github.com/nix-community/impermanence/blob/e337457502571b23e449bf42153d7faa10c0a562/nixos.nix#L197
-    # In the above example, the ${name} it is given is `persistif` because that is the attr that this type is being applied to (below).
-    # For it to find a value there, we need to populate that attr with *our* top level config
-    config.environment.persistence.persistif = config.persistif;
-  }).options.environment.persistence.type.nestedTypes.elemType;
+  mkIfElse = p: yes: no: lib.mkMerge [
+    (lib.mkIf p yes)
+    (lib.mkIf (!p) no)
+  ];
 in
 {
-  imports = [ inputs.impermanence.nixosModules.impermanence ];
+  imports = [
+    inputs.impermanence.nixosModules.impermanence
+    (lib.mkAliasOptionModule [ "persistif" ] [ "environment" "persistence" cfg.persistPath ])
+  ];
   options = {
-    persistif = lib.mkOption { default = { }; type = persistOptionType; };
     impermanence.enable = lib.mkEnableOption "impermanence";
     impermanence.persistPath = lib.mkOption { type = lib.types.path; description = "The path to the persisted directory"; example = "/persist"; };
     impermanence.snapshotPath = lib.mkOption { type = lib.types.str; description = "The path of the blanking snapshot"; example = "zpool/root@blank"; };
   };
-  config = lib.mkIf (cfg.enable) {
-    # forward the peristif config to the impermanence config
-    environment.persistence."" = config.persistif;
+  config = mkIfElse cfg.enable
+    {
+      # persisted values may be necessary for boot
+      fileSystems.${cfg.persistPath}.neededForBoot = true;
 
-    # since we overwrite the storage path here, we don't need to name it above
-    persistif.persistentStoragePath = cfg.persistPath;
+      # rollback to the provided snapshot on each boot
+      boot.initrd.postResumeCommands = lib.mkAfter ''
+        echo "Rolling back to ZFS snapshot '${cfg.snapshotPath}'"
+        zfs rollback -r ${cfg.snapshotPath}
+        echo "Rollback complete"
+      '';
 
-    # persisted values may be necessary for boot
-    fileSystems.${cfg.persistPath}.neededForBoot = true;
-
-    # rollback to the provided snapshot on each boot
-    boot.initrd.postResumeCommands = lib.mkAfter ''
-      echo "Rolling back to ZFS snapshot '${cfg.snapshotPath}'"
-      zfs rollback -r ${cfg.snapshotPath}
-      echo "Rollback complete"
-    '';
-
-    # this is needed to allow the home module to set `allowOther`, so that sudo can access the mounts
-    programs.fuse.userAllowOther = true;
-  };
+      # this is needed to allow the home module to set `allowOther`, so that sudo can access the mounts
+      programs.fuse.userAllowOther = true;
+    }
+    {
+      persistif.enable = false;
+    };
 }
