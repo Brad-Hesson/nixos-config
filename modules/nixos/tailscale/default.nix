6a4cc8740fb8d9e05db87f66a4f0f4f34c1e05fb
Brad Hesson <brad.hesson@outlook.com>
2026-09-09 16:22:26 -0600

    tailscale fixes

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -1,9 +1,18 @@
 {
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "both";
+  services = {
+    tailscale = {
+      enable = true;
+      useRoutingFeatures = "both";
+    };
+    resolved.enable = true;
   };
   persistif.directories = [
     "/var/lib/tailscale"
   ];
+  # fix for issue with dns ordering
+  systemd.services.tailscaled = {
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+  };
+  networking.networkmanager.dns = "systemd-resolved";
 }
