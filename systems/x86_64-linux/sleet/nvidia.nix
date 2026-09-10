2b3c7233d9c0bbcef3dbec455c79e52bae76fece
Brad Hesson <brad.hesson@outlook.com>
2026-09-09 19:00:52 -0600

    switch to nvidia offload

--- systems/x86_64-linux/sleet/nvidia.nix
+++ systems/x86_64-linux/sleet/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: {
+{ config, ... }: {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -7,45 +7,30 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
 
-
-  # force things to run with the gpu
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    __NV_PRIME_RENDER_OFFLOAD = "1";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # OpenGL/XWayland
-    __VK_LAYER_NV_optimus = "NVIDIA_only"; # Vulkan
-  };
+  services.switcherooControl.enable = true;
 
   hardware.nvidia = {
-    # Modesetting is required.
     modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.beta;
 
     prime = {
-      sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
+
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
     };
   };
 }
