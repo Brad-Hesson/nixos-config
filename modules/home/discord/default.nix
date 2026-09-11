8351171e40dbf211756c6fc69cec306bdfb055b7
Brad Hesson <brad.hesson@outlook.com>
2026-09-10 22:18:01 -0600

    fixed discord warning

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -3,4 +3,14 @@
   persistif.directories = [
     ".config/discord"
   ];
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    genericName = "Instant Messenger";
+    comment = "All-in-one cross-platform voice and text chat for gamers";
+    exec = "Discord %u";
+    icon = "discord";
+    categories = [ "Network" "InstantMessaging" ];
+    mimeType = [ "x-scheme-handler/discord" ];
+    settings.StartupWMClass = "discord";
+  };
 }
